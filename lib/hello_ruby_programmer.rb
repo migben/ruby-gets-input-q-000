d5d8c97ff947483e6b92cc07c859595c6fe4cecd
Miguel Ben <codexheartdevs@gmail.com>
2019-11-20 10:59:36 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -2,8 +2,5 @@
 
 def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
-  
-  puts "Please enter your name so that we can greet you more personally:"
-  
-  name = gets.chomp
+
 end
